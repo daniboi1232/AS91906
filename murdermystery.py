1c91b96309ae9cd19846cc7516932139a39db50f
daniboi1232 <112357371+daniboi1232@users.noreply.github.com>
2024-03-15 14:09:35 +1300

    added NPC and mapgui class

--- murdermystery.py
+++ murdermystery.py
@@ -3,7 +3,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 import os
 
-##temporary map used for player testing
+##temporary map used for player movement testing
 loc_list = [
     {"location": "A", "dest": ["B", "C", "D", "E", "F", "G"]},
     {"location": "B", "dest": ["C", "D", "E", "F", "G"]},
@@ -14,25 +14,32 @@ loc_list = [
     {"location": "G", "dest": []}
 ]
 
-#Player class
+### Player Class ###
 class Player:
     def __init__(self,name,location):
         self._name = name
         self._location = location
+        #self._item = item
 
     def __str__(self):
         return  f'The player, {self._name}, is currently in {self._location}'
     
-    #def name
+    ##Decorators##
+    #getter - shown as @property - is used for returning a variable
+    #setter - shown as @method.setter - is used for changing a variable
+    #deleter - shown as @method.deleter - is used for removing a variables contents
+
+    #def name for returning contents of variable
     @property
     def name(self): 
         return self._name
 
+    #Change the name to newname
     @name.setter
     def name(self,newname):
         self.name = newname
 
-    #def location
+    #def location for returning contents of variable
     @property
     def location(self):
         return self._location
@@ -41,6 +48,16 @@ class Player:
     def location(self,newloc):
         self.location = newloc
 
+    # @property
+    # def item(self):
+    #     return self._item
+
+    # @item.setter
+    # def item(self):
+    #     pass
+
+
+    #def move to change the location of player based on players input
     def move(self):
         dest = str(input('Where do you want to move? ')).upper()
         for loc in loc_list:
@@ -48,37 +65,53 @@ class Player:
                 if dest in loc["dest"]:
                     self._location = dest
                     return dest
-        return False
+        move_error = "Sorry But You Cannot Move There"
+        return move_error
 
 
 #test player
-bob = Player("Bob","A")
+bob = Player("Bob","F")
 print(str(bob))
 print(bob.move())
 
 
+### NPC Class ###
+class NPC:
+    def __init__(self, name, location, item):
+        self.name = name
+        self._location = location
+        self._item = item
+    
+    def __str__(self):
+        return "this is an npc"
+    
 
 
-#location class
+### Location Class ###
 class Loc:
-    def __init__():
+    def __init__(self, ):
         pass
 
     def __str__():
+        return ""
+
+
+class mapgui:
+    def __init__(self):
         pass
 
 
 ##testing printing of image for future map
-# root = Tk()
-# root.title("Tkinter Image test")
-# root.geometry('600x400')
-# img = ImageTk.PhotoImage(Image.open("wavestest.jpeg"))
+root = Tk()
+root.title("Tkinter Image test")
+root.geometry('600x400')
+img = ImageTk.PhotoImage(Image.open("wavestest.jpeg"))
 
-# ##defining the panel sizes
-# panel = Label(root, image = img)
-# panel.pack(side = "left", expand = "False")
+##defining the panel sizes
+panel = Label(root, image = img)
+panel.pack(side = "left", expand = "False")
 
-# ##runs root loop
-# root.mainloop()
+##runs root loop
+root.mainloop()
 
 
